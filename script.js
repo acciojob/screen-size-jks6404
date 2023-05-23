8449a25370e3d8c0167a602446e448bf3b1a0cc1
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-05-23 17:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const screensize = document.getElementById("sizeInfo");
+const width = sizeInfo.querySelector("h1:first-child");
+const height = sizeInfo.querySelector("h1:last-child");
+// screensize.innerText = `Width: ${expectedWidth} and Height: ${expectedHeight}`;
+// document.node.appendChild(screensize);
+
+function setScreenSize() {
+width.innerText = `Width: ${window.innerWidth}px`;
+height.innerText = `Height: ${window.innerHeight}px`;
+}
+window.addEventListener('resize', setScreenSize);
+setScreenSize()
